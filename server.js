c61ca8ab15caef63c475b766c3760807d7d095b8
Satyam Rajput <satyamrajput081998@gmail.com>
2025-05-09 03:18:45 -0400

    make a new API under20mmininners

--- server.js
+++ server.js
@@ -560,6 +560,37 @@ const Webbyawardwinnerinners = new mongoose.Schema({
   },
 });
 
+const Under20mmininners = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    videoUrlId: {
+      type: String,
+      required: true,
+    },
+    youtubeUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
 const SidebarItem = mongoose.model("Sidebars", sidebarItemSchema);
 const PodcastTreding = mongoose.model("trends", podcastSchema);
 const Webbyawards = mongoose.model("webbyawards", webbyAwards);
@@ -597,6 +628,8 @@ const Webbyawardwinnerinner = mongoose.model(
   "webbyawardwinnerinners",
   Webbyawardwinnerinners
 );
+
+const Under20mmininner = mongoose.model("under20mmininners", Under20mmininners);
 
 app.get("/api/trending", async (req, res) => {
   try {
@@ -812,6 +845,17 @@ app.get("/api/discovertrendinginner", async (req, res) => {
 app.get("/api/webbyawardwinnerinners", async (req, res) => {
   try {
     const podcasts = await Webbyawardwinnerinner.find();
+    console.log(podcasts);
+    res.json(podcasts);
+  } catch (err) {
+    console.error("Error fetching podcasts:", err);
+    res.status(500).send("Error fetching SidebarItem");
+  }
+});
+
+app.get("/api/under20mmininners", async (req, res) => {
+  try {
+    const podcasts = await Under20mmininner.find();
     console.log(podcasts);
     res.json(podcasts);
   } catch (err) {
